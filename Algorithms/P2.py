a1dbb1fcc0b271891f22607f38b5b4814cab5773
deallynomore <xianyiting@gmail.com>
2016-08-19 09:55:00 +0800

    add array

--- Algorithms/P2.py
+++ Algorithms/P2.py
@@ -27,9 +27,30 @@ def search_num(nums, val):
             idx.append(i)
     return idx
 
+# P12 2.1-4
+def add_binary(A, B):
+    C = [0 for i in range(len(A)+1)]
+    flag = 0
+    for i in range(len(A)):
+        if A[i]+B[i]+flag==2:
+            C[i] = 0
+            flag = 1
+        elif A[i]+B[i]+flag==1:
+            C[i] = 1
+            flag = 0
+        elif A[i]+B[i]+flag==3:
+            C[i] = 1
+            flag = 1
+        else:
+            C[i] = 0
+            flag = 0
+    C[-1] = flag
+    return C
+
 
 
 if __name__ == '__main__':
     nums = [2,1,5,6,8]
     print insert_sort(nums, isIncrease=1)
     print search_num(nums, 8)
+    print add_binary([1,0,0,1],[1,1,1,1])
